e59991dccb295766ec456630810cb75e23867678
Alejandro P. Prenda <aleplipre@hotmail.com>
2025-07-23 21:55:08 +0200

    Segunda encuesta histórica

--- Registro.sql
+++ Registro.sql
@@ -55,38 +55,54 @@ CREATE TABLE calificaciones(
 /* Introducción de datos: */
 -- Autores:
 INSERT INTO autores (autor, fecha_nacimiento)
-VALUES ('R. F. Kuang', '1996-05-29'),       -- 1
-       ('Katsuhiro Otomo', '1954-04-14'),   -- 2
-       ('Arthur C. Clarke', '1917-12-16'),  -- 3
-       ('Brandon Sanderson', '1975-12-19'), -- 4
-       ('Frank Herbert', '1920-10-08'),     -- 5
-       ('Stephen King', '1947-09-21');      -- 6
+VALUES ('R. F. Kuang', '1996-05-29'),           -- 1
+       ('Katsuhiro Otomo', '1954-04-14'),       -- 2
+       ('Arthur C. Clarke', '1917-12-16'),      -- 3
+       ('Brandon Sanderson', '1975-12-19'),     -- 4
+       ('Frank Herbert', '1920-10-08'),         -- 5
+       ('Stephen King', '1947-09-21'),          -- 6
+       ('Nieves Concostrina', '1961-08-01'),    -- 7
+       ('Susanna Clarke', '1959-11-01'),        -- 8
+       ('Terry Pratchett', '1948-04-28');       -- 9
 -- Géneros:
 INSERT INTO generos (genero)
 VALUES ('Fantasía'),        -- 1
        ('Young adult'),     -- 2
        ('Ciencia ficción'), -- 3
        ('Aventuras'),       -- 4
-       ('Drama');           -- 5
+       ('Drama'),           -- 5
+       ('Terror'),          -- 6
+       ('Suspense'),        -- 7
+       ('Crimen'),          -- 8
+       ('Histórica'),       -- 9
+       ('Humor'),           -- 10
+       ('Divulgación');     -- 11
 -- Libros:
 INSERT INTO libros (titulo, fecha_publicacion, paginas, dia_en_que_se_comento)
-VALUES ('La guerra de la amapola', 2018, 646, '2024-12-29'),    -- 1
-       ('Akira', 1990, NULL, '2021-06-09'),                     -- 2
-       ('2001: Una odisea espacial', 1968, 240, '2021-07-21'),  -- 3
-       ('El imperio final', 2006, 690, '2021-09-21'),           -- 4
-       ('Dune', 1965, 658, '2021-12-01'),                       -- 5
-       ('La milla verde', 1996, 444, '2025-02-16');             -- 6
+VALUES ('La guerra de la amapola', 2018, 646, '2024-12-29'),                                                            -- 1
+       ('Akira', 1990, NULL, '2021-06-09'),                                                                             -- 2
+       ('2001: Una odisea espacial', 1968, 240, '2021-07-21'),                                                          -- 3
+       ('El imperio final', 2006, 690, '2021-09-21'),                                                                   -- 4
+       ('Dune', 1965, 658, '2021-12-01'),                                                                               -- 5
+       ('La milla verde', 1996, 444, '2025-02-16'),                                                                     -- 6
+       ('Mr. Mercedes', 2014, 437, '2022-01-07'),                                                                       -- 7
+       ('Pretérito imperfecto: Historias del mundo desde el año de la pera hasta ya mismo', 2018, 451, '2022-02-15'),   -- 8
+       ('Jonathan Strange y el Señor Norrell', 2004, 795, '2022-04-28'),                                                -- 9
+       ('La ciencia de Mundodisco', 1999, 416, '2022-06-16'),                                                           -- 10
+       ('Cita con Rama', 1973, 448, '2025-04-08');                                                                      -- 11
 -- SELECT * FROM libros;                        -- Ver id_libro.
 -- SELECT * FROM autores;                       -- Ver id_autor.
 INSERT INTO libros_autores (id_libro, id_autor)
-VALUES (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6);
+VALUES (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 6), (8, 7), (9, 8), (10, 9), (11, 3);
 -- SELECT * FROM generos ORDER BY id_genero;    -- Ver id_genero.
 INSERT INTO libros_generos (id_libro, id_genero)
-VALUES (1, 1), (1, 2), (2, 3), (3, 3), (4, 1), (4, 2), (5, 3), (5, 4), (6, 1), (6, 5);
+VALUES (1, 1), (1, 2), (2, 3), (3, 3), (4, 1), (4, 2), (5, 3), (5, 4), (6, 1), (6, 5), (7, 6), (7, 7), (7, 8), (8, 9),
+       (8, 10), (9, 1), (9, 9), (10, 11), (10, 10), (10, 1), (11, 3);
 -- Calificaciones:
 INSERT INTO calificaciones (id_libro, estrellas_1, estrellas_2, estrellas_3, estrellas_4, estrellas_5)
 VALUES (1, 0, 3, 6, 1, 1), (2, 0, 0, 1, 2, 1), (3, 0, 1, 1, 1, 1), (4, 1, 2, 4, 0, 0), (5, 0, 0, 2, 4, 3),
-       (6, 0, 0, 1, 6, 1);
+       (6, 0, 0, 1, 6, 1), (7, 0, 1, 0, 2, 0), (8, 0, 1, 2, 0, 0), (9, 0, 0, 1, 0, 0), (10, 1, 2, 0, 0, 0),
+       (11, 0, 2, 3, 1, 0);
 
 /* Visualización de datos: */
 CREATE VIEW info_libros AS
